14b1b7f0dfe8b4cc82c3917db900d1b88bdb8192
Acka1 <Loanmarjouxpro@gmail.com>
2022-12-10 10:28:48 +0100

    v0.1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//dark mode
 const checkbox = document.querySelector("#flexSwitchCheckDefault");
 
 const body = document.querySelector("body");
@@ -10,3 +12,117 @@ checkbox.addEventListener('click', function() {
         titre.classList.toggle("darkmode");
     })
 });
+
+//local storage
+//let input = document.getElementById("searchInput");
+
+// function save() {  
+//      let iframe = document.getElementById("searchInput").value  
+//    window.localStorage.setItem("iframe" + (+new Date()), iframe);   
+//      window.location.reload() 
+// };
+
+
+// //récup clé les transfo en tableau
+// window.onload = function load (){
+// let iframes = {...localStorage};
+
+// let html = "";
+
+// for ([key, iframe] of Object.entries(iframes)) {
+// html += iframe + `<button class="delete" style="position:relative;left-39px;top-10px" data-keys="${key}"></button>`
+
+// }
+
+
+// document.querySelector('.video-play').innerHTML = html;
+
+// document.querySelectorAll('button.delete').forEach((button) => {
+//     button.addEventListener('click', () => {
+// window.localStorage.removeItem(button.database.key);
+// window.location.reload()
+
+//     })
+    
+// })
+
+// }
+
+// 1. Enregistrer une video youtube dans le localStorage //
+function save() {
+  let iframe = document.getElementById("iframeCode").value
+  window.localStorage.setItem("iframe" + (+new Date()), iframe);
+  window.location.reload()
+};
+
+// 2. Chargement des vidéos déjà enregistrées //
+window.onload = function load() {
+  // Transformer le LocalStorage en objet clé:valeur que l'on peut lire facilement
+  let iframes = {...localStorage};
+
+  let html = "";
+
+  // Transformer les iframes en tableau, en faire une seule chaine de caractère géante, 
+  // puis ajouter le bouton de suppression après chaque iframe avec un attribut contenant la clé
+  for([key, iframe] of Object.entries(iframes)) {
+      html += iframe + `<button class="delete" style="position:relative;left:-30px;top:-10px" data-key="${key}">X</button>`
+  }
+
+  // Ajouter le code HTML généré dans le div vide
+  document.querySelector('.video-play').innerHTML = html; 
+
+  // Enregistrer l'événement de suppression de vidéo
+  document.querySelectorAll('button.delete').forEach((button) => {
+
+      // Au clic sur le bouton de suppression
+      button.addEventListener("click", () => {
+          // Suppression effective de l'iframe dans le localStorage
+          window.localStorage.removeItem(button.dataset.key);
+          // Recharger la page pour voir les changements
+          window.location.reload();
+      })
+
+  })
+}
+
+// code du prof 
+
+
+
+
+// // 1. Enregistrer une video youtube dans le localStorage //
+// function save() {
+//     let iframe = document.getElementById("iframeCode").value
+//     window.localStorage.setItem("iframe" + (+new Date()), iframe);
+//     window.location.reload()
+// };
+
+// // 2. Chargement des vidéos déjà enregistrées //
+// window.onload = function load() {
+//     // Transformer le LocalStorage en objet clé:valeur que l'on peut lire facilement
+//     let iframes = {...localStorage};
+
+//     let html = "";
+
+//     // Transformer les iframes en tableau, en faire une seule chaine de caractère géante, 
+//     // puis ajouter le bouton de suppression après chaque iframe avec un attribut contenant la clé
+//     for([key, iframe] of Object.entries(iframes)) {
+//         html += iframe + <button class="delete" style="position:relative;left:-30px;top:-10px" data-key="${key}">X</button>
+//     }
+
+//     // Ajouter le code HTML généré dans le div vide
+//     document.querySelector('.video-play').innerHTML = html; 
+
+//     // Enregistrer l'événement de suppression de vidéo
+//     document.querySelectorAll('button.delete').forEach((button) => {
+
+//         // Au clic sur le bouton de suppression
+//         button.addEventListener("click", () => {
+//             // Suppression effective de l'iframe dans le localStorage
+//             window.localStorage.removeItem(button.dataset.key);
+//             // Recharger la page pour voir les changements
+//             window.location.reload();
+//         })
+
+//     })
+//}
